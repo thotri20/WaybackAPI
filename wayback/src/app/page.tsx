9407bdb246ccbdc7a5c97a3de9d7807bf33f677e
Isac Ledel <IRLTrooper>
2025-04-24 13:16:16 +0200

    fixed size, added date search

--- wayback/src/app/page.tsx
+++ wayback/src/app/page.tsx
@@ -2,8 +2,8 @@ import WayBack from '@/components/WayBack'
 
 export default function Home() {
   return (
-    <main className="p-6 max-w-xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Wayback Time Machine</h1>
+    <main className="p-6 mx-auto">
+      <h1 className="text-2xl text-center font-bold mb-4">Wayback Time Machine</h1>
       <WayBack />
     </main>
   )
